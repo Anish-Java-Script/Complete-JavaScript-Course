ff5e8099c60b69e7dd9d73334c2b8c93b4728148
anish <anishgharti10@gmail.com>
2023-09-27 18:24:15 +0545

    feat:developer skills

--- javaScriptFundamentals-02/script.js
+++ javaScriptFundamentals-02/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
 ///////////////////////////////////////
@@ -590,31 +590,27 @@ console.log(calcAverage(tips));
 
 // continue and break
 
-
 const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-  }
-  
-  
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const totals = [];
 
-
-for( let i = 0; i<= bills.length -1; i++){
-    tips.push(calcTip(bills[i]));
-    totals.push(bills[i] + tips[i])
+for (let i = 0; i <= bills.length - 1; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
 }
-console.log(tips);
+console .log(tips);
 console.log(totals);
 
-
 var sum = 0;
-var calcAvg = function(arr){
-    for(let i=0; i<arr.length; i++){
-        sum = sum + arr[i];
-    }
-    return sum/arr.length;
-}
+var calcAvg = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum / arr.length;
+};
 
-console.log(calcAvg([1,2,3,4,5]));
+console.log(calcAvg([1, 2, 3, 4, 5]));
