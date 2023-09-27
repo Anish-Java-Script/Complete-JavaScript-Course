aecb0f070d398119a9a42827bd90c272939280f9
anish <anishgharti10@gmail.com>
2023-09-27 23:37:20 +0545

    feat:conpleted with keydown button

--- 005 Modal/script.js
+++ 005 Modal/script.js
@@ -6,22 +6,24 @@ const overlay = document.querySelector(".overlay");
 const btlCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener("click", function () {
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-  });
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener("click", openModal);
+btlCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-  btlCloseModal.addEventListener('click', function(){
-    modal.classList.add('hidden');
-    overlay.classList.add   ('hidden');
 
-  });
-  overlay.addEventListener('click', function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-
-  });
-  
-}
+document.addEventListener('keydown', function(e){
+  console.log(e.key);
+  if(e.key == 'Escape' && !modal.classList.contains('hidden')){
+    closeModal();
+  }
+});
